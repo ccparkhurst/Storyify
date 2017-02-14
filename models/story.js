a06358bc0da35dde0a4f8d805c36965061d43bb8
Christina Parkhurst <ccparkhurst@users.noreply.github.com>
2017-02-14 11:20:49 -0500

    cleaned comments

--- models/story.js
+++ models/story.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
-//NEED TO Add key/value for story title
+
 var StorySchema = new mongoose.Schema({
   title:          { type: String },
   name:           { type: String,  required: true },
   animal:         { type: String,  required: true },
   place:          { type: String,  required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref:'User'}// added in user for passport and mongoose to use to match users to their stories
+  user: { type: mongoose.Schema.Types.ObjectId, ref:'User'}
     },
-  { timestamps: true }  // createdAt, updatedAt
+  { timestamps: true }  
 );
 
 module.exports = mongoose.model('Story', StorySchema);
