ca0067bf0e142a09819175d627625e2e24ef053a
Christina Parkhurst <ccparkhurst@users.noreply.github.com>
2017-02-14 11:26:38 -0500

    cleaned comments

--- public/js/stories/storyEdit/storyEdit.component.js
+++ public/js/stories/storyEdit/storyEdit.component.js
@@ -1,7 +1,7 @@
 myApp.component('storyEdit', {
 	templateUrl: '/js/stories/storyEdit/storyEdit.html',
 	controller: function(storyService, $state, $stateParams) {
-		// put a helpful comment to why we did this
+
 		this.story = null;
 
 		this.show = function() {
@@ -21,25 +21,3 @@ myApp.component('storyEdit', {
 		});
 	}
 });
-
-// // Edit Story Controller
-// myApp.controller('editCtrl', function(storyService, $state, $stateParams) {
-// 		// put a helpful comment to why we did this
-// 		this.story = null;
-
-// 		this.show = function() {
-// 			$state.go('storyShow', { id: this.story._id });
-// 		};
-
-// 		this.save = function() {
-// 			storyService.update(this.story)
-// 			.then( res => {
-// 				$state.go('storyShow', { id: this.story._id });
-// 			});
-// 		};
-
-// 		storyService.getStory($stateParams.id)
-// 		.then( res => {
-// 			this.story = res.data;
-// 		});
-// });
